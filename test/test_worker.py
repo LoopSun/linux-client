83fb6f08082ff465944514fc0b3ce73d02312796
loopsun <dave@grid-safe.com>
2017-01-10 18:20:02 +0800

    commit code

--- test/test_worker.py
+++ test/test_worker.py
@@ -1,13 +1,19 @@
 #encoding=utf-8
 
+from time import sleep
+
 from test.test_funs import count_words_at_url
+
 
 if __name__ == '__main__':
     from rq import Queue, use_connection
     from utils.worker import redis_connection
 
     use_connection(redis_connection)
-    q = Queue()
+    q = Queue("low")
 
     result = q.enqueue(count_words_at_url, "https://www.heroku.com/")
+    print(result.result)
+    sleep(5)
+    print(result.result)
 
